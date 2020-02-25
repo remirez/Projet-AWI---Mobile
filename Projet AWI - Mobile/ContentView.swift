4a349568391983a98442dd65f6cb1500250bd385
Remy McConnell <remy.m@floversal.com>
2020-02-25 16:24:34 +0100

    Initial Commit

---
+++ Projet AWI - Mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Projet AWI - Mobile
+//
+//  Created by Remy McConnell on 2/25/20.
+//  Copyright © 2020 Remy McConnell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
